0024291bc579f0073b9cbb105ccc4135d3d48539
caijiayou <ab881027@yahoo.com.tw>
2022-07-19 15:59:13 +0800

    Update gps.py

--- sub/GPS.py
+++ sub/GPS.py
@@ -1,21 +1,53 @@
-from operator import index
-import os
+import pynmea2
+import serial
+import time
+import string
 
-from matplotlib.pyplot import title
-os.system('cls')
 
-data = "$GPGGA,121253.000,3937.3090,N,11611.6057,E,1,06,1.2,44.6,M,-5.7,M,,0000*72"
+def gps():
+    while 1:
+        try:
+            #get GPS_data
+            port = "/dev/ttyAMA0"
+            ser = serial.Serial(port,baudrate=9600,timeout=0.5)
+            dataout = pynmea2.NMEAStreamReader()
+            newdata = ser.readline()
 
-t = data[data.index('$'): data.index(',')]
-print('title: ', t)
+            #cut GPS_data
+            if newdata[0:6]== b"$GNRMC":
+                print('\n'*3)
+                gps = newdata[newdata.index(b","): newdata.index(b"E")]
+                lat = gps[gps.index(b"A")+2: gps.index(b"N")-1]
+                lon = gps[gps.index(b"N")+2: len(gps)-1]
 
-if t=='$GPGGA':
-    gps = data[data.index(',')+1: data.index('N')-1]
-    
-    # longitude and latitude
-    longitude = gps[0: gps.index(',')]
-    latitude = gps[gps.index(',')+1: len(gps)]
+                #data bytes to string
+                gps = bytes.decode(gps)
+                lat = bytes.decode(lat)
+                lon = bytes.decode(lon)
+                print('GPS: ', gps)
+                print('latitude: ', lat)
+                print('longitude: ', lon)
 
-print('gps(longitude and latitude): ', gps)
-print('longitude: ', longitude)
-print('latitude: ', latitude)
+                #latitude & longitude unit conversion
+                f_lat = lat[2:]
+                d_lat = lat[:2]
+                d_lat = float(f_lat)/60 + float(d_lat)
+
+                f_lon = lon[3:]
+                d_lon = lon[:3]
+                d_lon = float(f_lon)/60 + float(d_lon)
+                print('========================================')
+                print('latitude: ', d_lat)
+                print('longitude: ', d_lon)
+                print('========================================')
+                break
+        except:
+            print('Not connected')
+            time.sleep(0.3)
+            continue
+
+if __name__==('__main__'):
+    while True:
+        gps()
+        
+
